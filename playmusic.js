93b3c6cd91c3a342f2af0808259ed147b2325d9a
xiachedann <2436452144@qq.com>
2024-08-03 01:16:49 +0800

    Update playmusic.js

--- playmusic.js
+++ playmusic.js
@@ -3,6 +3,20 @@ function isMobile() {
 	var userAgent = navigator.userAgent.toLowerCase();
 	return (/iphone|ipad|ipod|android|windows phone/.test(userAgent));
 }
+// 检测是否在移动端 QQ 浏览器中访问
+function isMobileQQBrowser() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    return (userAgent.indexOf('QQ') !== -1 && userAgent.indexOf('Mobile') !== -1);
+}
+
+// 使用例子
+if (isMobileQQBrowser()) {
+    console.log('当前环境是移动端 QQ 浏览器');
+} else {
+   window.location.href = '/feedback.html';
+}
+
+
 
 // 如果不在移动端访问，则跳转到提示页面
 if (!isMobile()) {
