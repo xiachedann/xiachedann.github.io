bfaeede8a89504673f6de66d3b9a690a6fde6daa
xiachedann <2436452144@qq.com>
2024-08-03 01:11:59 +0800

    Update playmusic.js

--- playmusic.js
+++ playmusic.js
@@ -1,3 +1,15 @@
+// 判断是否在移动端访问
+function isMobile() {
+	var userAgent = navigator.userAgent.toLowerCase();
+	return (/iphone|ipad|ipod|android|windows phone/.test(userAgent));
+}
+
+// 如果不在移动端访问，则跳转到提示页面
+if (!isMobile()) {
+ 	window.location.href = '/404';
+}
+
+
 function loadhtml(url){ 
  var xhr = new XMLHttpRequest;
  var html = null;
